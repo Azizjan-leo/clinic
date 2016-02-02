3f49746f9b1740d60c5008f1bb1216552132abd8
Sedokina <lll.allino4ka.lll@gmail.com>
2016-02-02 16:27:53 +0600

    db

---
+++ system.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.6
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 02 2016 г., 12:46
+-- Версия сервера: 5.5.41-log
+-- Версия PHP: 5.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Diagnoses`
+--
+
+CREATE TABLE IF NOT EXISTS `Diagnoses` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(30) DEFAULT NULL,
+  `Description` varchar(100) DEFAULT NULL,
+  `Blood_test` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Employee`
+--
+
+CREATE TABLE IF NOT EXISTS `Employee` (
+  `Emp_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `First_Name` varchar(30) DEFAULT NULL,
+  `Middle_Name` varchar(30) DEFAULT NULL,
+  `Second_Name` varchar(30) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  `Prof` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Emp_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `patients`
+--
+
+CREATE TABLE IF NOT EXISTS `patients` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `First_name` varchar(30) DEFAULT NULL,
+  `Middle_name` varchar(30) DEFAULT NULL,
+  `Second_name` varchar(30) DEFAULT NULL,
+  `Gender` varchar(10) DEFAULT NULL,
+  `Date` date DEFAULT NULL,
+  `Adress` varchar(100) DEFAULT NULL,
+  `Card_ID` int(11) DEFAULT NULL,
+  `Care_Doctor` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Staff`
+--
+
+CREATE TABLE IF NOT EXISTS `Staff` (
+  `Staff_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` int(11) DEFAULT NULL,
+  `Salary` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Staff_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Symptoms`
+--
+
+CREATE TABLE IF NOT EXISTS `Symptoms` (
+  `Symptom_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` int(11) DEFAULT NULL,
+  `Description` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Symptom_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
