051bd92a77a580b0b5c7fc3fd9dedcad9210e3db
Azizjan-leo <litpulla@mail.ru>
2016-02-02 16:23:00 +0600

    Folders and index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+	</head>
+	<body>
+	
+	</body>
+</html>
