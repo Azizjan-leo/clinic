96d86e240f54e373d81bf8be834561e50f5a0b5d
Azizjan-leo <litpulla@mail.ru>
2016-02-07 08:27:00 +0600

    Система выведена в отдельную страницу

--- pages/system/newSymptom.php
+++
@@ -1,4 +0,0 @@
-<form action="../../phpHendlers/forms/newSymptom.php" method="post">
-Name: <input type="text" name="name"><br>
-Description: <textarea name ="desc" cols="40" rows="10"></textarea>
-</form>
