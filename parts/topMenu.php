4c443232f0f30e4e8e740ae93a6fb91976c2188c
Sedokina <lll.allino4ka.lll@gmail.com>
2016-02-11 22:13:19 +0600

    Doc page

--- parts/topMenu.php
+++ parts/topMenu.php
@@ -1,15 +1,15 @@
 ﻿<?
 	printf ('<nav id="menu-wrap">
       <ul id="menu">
-       <li><a href="#">Doctors</a>
-        <ul>
+       <li><a href="#">Doctors</a></li>
+       <li><a href="#">Меню</a>
+       <ul>
          <li><a href="#">Подменю</a></li>
          <li><a href="#">Подменю</a></li>
          <li><a href="#">Подменю</a></li>
          <li><a href="#">Подменю</a></li>
         </ul>
        </li>
-       <li><a href="#">Меню</a></li>
        <li><a href="../index.php?content=%s">Expert system</a></li>
        <li><a href="../phpHendlers/connect.php?action=%s">Out</a></li>
       </ul>
