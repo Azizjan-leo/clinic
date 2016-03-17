59893dac3518639b74f5cb6139eccfc105baf2b1
Azizjan-leo <litpulla@mail.ru>
2016-03-17 08:11:10 +0700

    To undo last push

--- phpHendlers/forms/comment_gen.php
+++
@@ -1,8 +0,0 @@
-<?php
- include("../connect.php");
- $text_comment = $_POST["text_comment"];
- $comment_date = date("y.m.d");
- $text_comment = htmlspecialchars($text_comment);
- mysql_query("INSERT INTO `Comments_general` (`Comment_ID`, `Date`, `Text`) VALUES ('', '".$comment_date."', '".$text_comment."')");
- header("Location: ".$_SERVER["HTTP_REFERER"]);
- ?>
